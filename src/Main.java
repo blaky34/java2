9c01767d5fc367e839e7e3bfc698fff7df223cd4
Ксения <ksenia@MacBook-Air-Ksenia.local>
2022-04-24 17:24:28 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 2000;
+        int bonusMiles = 1;
+        int rubles = 20;
+        int totalBonus = cost * bonusMiles / rubles;
+        System.out.println(totalBonus);
+    }
+
+}
